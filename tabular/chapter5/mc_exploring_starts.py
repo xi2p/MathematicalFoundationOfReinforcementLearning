52bdf90a54798456411a26c50921b1af2f4ea004
xi2p <z1740067601@163.com>
2026-02-12 12:32:14 +0800

    refactor: combine tabular and function part of the mforl lib into one lib

--- tabular/chapter5/mc_exploring_starts.py
+++
@@ -1,2 +0,0 @@
-# It is hard to find a starting state-action pair that ensure all state-action pairs are visited(exploring starts condition).
-# So we skip the code implementation for exploring starts method.
